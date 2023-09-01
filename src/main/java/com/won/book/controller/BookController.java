2cb0e32f842567e7ae6cf7457757c64687cb4ecf
joyj10 <micool1030@gmail.com>
2023-09-01 22:00:14 +0900

    book & readBook controller path 변경

--- src/main/java/com/won/book/controller/BookController.java
+++ src/main/java/com/won/book/controller/BookController.java
@@ -18,25 +18,24 @@ import java.util.List;
  * @version 1.0,
  */
 @RestController
-@RequestMapping("/book")
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping
+    @GetMapping("/v1//book")
     public ResultResponse<List<BookVo>> getBooks(@RequestParam String keyword) {
         List<BookVo> bookVos = bookService.getBooks(keyword);
         return new ResultResponse<>(bookVos);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/v1//book/{id}")
     public ResultResponse<BookVo> getBook(@PathVariable("id") Long bookId) {
         BookVo bookVo = bookService.getBook(bookId);
         return new ResultResponse<>(bookVo);
     }
 
-    @PostMapping
+    @PostMapping("/v1//book")
     public ResultResponse<Long> save(@RequestBody BookCreateRequest bookCreateRequest) {
         // 책 저장 : 추후 고려 필요(책을 등록한 사람의 책만 보여지도록 할 것인지!)
         return new ResultResponse<>(bookService.save(bookCreateRequest));
--- src/main/java/com/won/book/controller/ReadBookContentController.java
+++ src/main/java/com/won/book/controller/ReadBookContentController.java
@@ -5,29 +5,28 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/book/read/content")
 @Api(tags = "3. Read Book content Controller")
 public class ReadBookContentController {
 
-    @GetMapping
+    @GetMapping("/vi/book/read/content")
     @ApiOperation(value = "읽은 책 글귀 리스트")
     public String getList(String str) {
         return "Hello, " + str;
     }
 
-    @PostMapping
+    @PostMapping("/vi/book/read/content")
     @ApiOperation(value = "읽은 책 글귀 저장 하기")
     public String save(String str, String str2) {
         return "Hello, " + str;
     }
 
-    @PatchMapping("/{content-id}")
+    @PatchMapping("/vi/book/read/content/{content-id}")
     @ApiOperation(value = "읽은 책 글귀 수정 하기")
     public String update(@PathVariable("content-id") Long contentId, String str) {
         return "Hello, " + str;
     }
 
-    @DeleteMapping("/{content-id}")
+    @DeleteMapping("/vi/book/read/content/{content-id}")
     @ApiOperation(value = "읽은 책 글귀 삭제 하기")
     public String delete(@PathVariable("content-id") Long contentId, String str) {
         return "Hello, " + str;
