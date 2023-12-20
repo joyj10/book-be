e780c3a6d602722053bed53fac59d9a67c839553
joyj10 <micool1030@gmail.com>
2023-12-20 13:16:49 +0900

    [feat] Log interceptor setting

---
+++ src/main/java/com/won/book/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.won.book.common.config;
+
+import com.won.book.common.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
+}
