85b4f3816322211e4c8a090e4969864126b64df9
joyj10 <micool1030@gmail.com>
2024-01-11 22:54:06 +0900

    [refactor] spring security 6 문법 수정

--- book-common/src/main/java/com/won/bookcommon/config/WebSecurityConfig.java
+++ book-common/src/main/java/com/won/bookcommon/config/WebSecurityConfig.java
@@ -4,37 +4,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+	private static final String[] whitelist = {
+			"/api/login",
+			"/api/logout",
+			"/api/v3/api-docs",
+			"/api/configuration/ui",
+			"/api/swagger-resources/*",
+			"/api/configuration/security",
+			"/api/swagger-ui.html",
+			"/api/swagger-ui/",
+			"/api/swagger-ui/*"
+	};
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("/api/v1/**").authenticated()
-                        .anyRequest().permitAll()
-                );
-
-        return http.build();
-	}
-
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return web -> web.ignoring().requestMatchers(
-				"/api/v3/api-docs",
-				"/api/configuration/ui",
-				"/api/swagger-resources/**",
-				"/api/configuration/security",
-				"/api/swagger-ui.html",
-				"/api/swagger-ui/",
-				"/api/swagger-ui/**",
-				"/webjars/**",
-				"/api/**",
-				"/logout"
-		);
+		// basic authentication
+		http.httpBasic(withDefaults());
+		// csrf
+		http.csrf(AbstractHttpConfigurer::disable);
+		// authorization
+		http.authorizeHttpRequests(requests -> requests
+						.requestMatchers(whitelist).permitAll()
+						.anyRequest().authenticated());
+		// login
+		http.formLogin(AbstractHttpConfigurer::disable);
+		return http.build();
 	}
 }
