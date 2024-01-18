22eb2ff0b3a0a209ee6636fc0fe7f7f13e9be2fe
joyj10 <micool1030@gmail.com>
2024-01-18 20:47:39 +0900

    [fix] LogInterceptor 불필요 로그 삭제

--- book-common/src/main/java/com/won/bookcommon/interceptor/LogInterceptor.java
+++ book-common/src/main/java/com/won/bookcommon/interceptor/LogInterceptor.java
@@ -1,11 +1,10 @@
 package com.won.bookcommon.interceptor;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
 import java.util.UUID;
 
 @Slf4j
@@ -14,16 +13,11 @@ public class LogInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
-        String logId = UUID.randomUUID().toString();
+        String logId = UUID.randomUUID().toString().substring(0,8);
         request.setAttribute("logId", logId);
 
         log.info("REQUEST [{}][uri={}][{}]", logId, requestURI, handler);
         return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        log.info("modelAdnView={}", modelAndView);
     }
 
     @Override
