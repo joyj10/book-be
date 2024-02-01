091bb1c7a8ac90e87d2f5e2848c4433f2c4d4e80
joyj10 <micool1030@gmail.com>
2024-02-01 23:02:20 +0900

    [test] controller - 읽은 책 저장 실패 케이스 테스트 코드 추가 - valid 체크 실패 케이스 추가

--- book-app-api/src/test/java/com/won/bookappapi/api/controller/ReadBookControllerTest.java
+++ book-app-api/src/test/java/com/won/bookappapi/api/controller/ReadBookControllerTest.java
@@ -18,6 +18,8 @@ import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ReadBookController.class)
 class ReadBookControllerTest {
@@ -49,5 +51,74 @@ class ReadBookControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
         )
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @DisplayName("읽은 책 저장 시 읽은 날 데이터가 빈값인 경우 예외가 발생한다.")
+    @Test
+    @WithMockUser(username = "테스트")
+    void save_invalid_readAt() throws Exception {
+        // given
+        ReadBookCreateRequest request = ReadBookCreateRequest.builder()
+                .bookId(1L)
+                .readAt("")
+                .rating(5)
+                .reviews(List.of("재미 있는 책"))
+                .build();
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/book/read").with(csrf())
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.code").value("CB0001"));
+    }
+
+    @DisplayName("읽은 책 저장 시 평점이 범위에서 벗어나는 경우 예외가 발생한다.")
+    @Test
+    @WithMockUser(username = "테스트")
+    void save_invalid_rating() throws Exception {
+        // given
+        ReadBookCreateRequest request = ReadBookCreateRequest.builder()
+                .bookId(1L)
+                .readAt("2024-02-01")
+                .rating(10)
+                .reviews(List.of("재미 있는 책"))
+                .build();
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/book/read").with(csrf())
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.code").value("CB0001"));
+    }
+
+    @DisplayName("읽은 책 저장 시 bookId 값은 양수여야 한다.")
+    @Test
+    @WithMockUser(username = "테스트")
+    void save_invalid_bookId() throws Exception {
+        // given
+        ReadBookCreateRequest request = ReadBookCreateRequest.builder()
+                .bookId(-1L)
+                .readAt("2024-02-01")
+                .rating(3)
+                .reviews(List.of("재미 있는 책"))
+                .build();
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/v1/book/read").with(csrf())
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.code").value("CB0001"));
     }
 }
